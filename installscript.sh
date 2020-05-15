a69ddf88605957e796e7f596dac8dc7aaf41c836
Fede <fede.cuci@protonmail.com>
2020-05-15 16:08:24 +0200

    download dotfiles through https instead of ssh

--- installscript.sh
+++ installscript.sh
@@ -15,6 +15,7 @@ ssh-keygen -t ed25519 -C "fede.cuci@mailbox.org"
 
 # Add custom dependencies
 sudo add-apt-repository ppa:bashtop-monitor/bashtop # for bashtop
+
 echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list #Add Etcher debian repository
 sudo apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys 379CE192D401AB61 #Trust Bintray.com's GPG key
 sudo apt update 
@@ -50,10 +51,13 @@ sudo snap install authy --beta
 
 clear -x
 
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
 # Install all wanted apt programs
 printf "Installing apt packages...\n"
 sleep 2
-apt_packages=(wireshark hydra hydra-gtk aircrack-ng gdebi gnome-tweaks hashcat gparted nmap tor git neovim python3-pip tmux ripgrep dconf-editor mlocate gobuster qbittorrent mpv virtualenv timeshift vifm kazam feh gnome-boxes steam wireguard openvpn golang-go bashtop apache2 zsh gnome-shell-extensions curl);
+apt_packages=(xclip wireshark hydra hydra-gtk aircrack-ng gdebi gnome-tweaks hashcat gparted nmap tor git neovim python3-pip tmux ripgrep dconf-editor mlocate gobuster qbittorrent mpv virtualenv timeshift vifm kazam feh gnome-boxes steam wireguard openvpn golang-go bashtop apache2 zsh gnome-shell-extensions curl);
 
 for i in "${apt_packages[@]}"
 do
@@ -102,9 +106,23 @@ function ulauncher () {
     rm "ulauncher_${ulauncher_version}_all.deb";
 }
 
+# Create directory for neovim configuration
+mkdir $HOME/.config/nvim
+
 # Setup git configuration variables
 git config --global user.email "fede.cuci@mailbox.org"
 git config --global user.name "Federico Cucinotta"
+
+#Clone repo containing dotfiles
+git clone https://gitlab.com/FedeCuci/dotfiles.git $HOME/.dotfiles
+
+# Create symbolic links for dotfiles in the home directory
+ln -fs $HOME/.dotfiles/.bashrc $HOME/.bashrc
+ln -fs $HOME/.dotfiles/.zshrc $HOME/.zshrc
+ln -fs $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -fs $HOME/.dotfiles/init.vim $HOME/.config/nvim/init.vim
+
+git init $HOME/.dotfiles/
 
 # Run the functions above.
 #mega;
